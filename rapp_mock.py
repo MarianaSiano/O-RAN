ff272b96d009e9d2c8d909c4f9867e1f2c75f16e
MarianaSiano <marianasianop@gmail.com>
2026-01-07 16:09:10 -0300

    Atualizando o arquivo 'rapp_mock'

--- rapp_mock.py
+++ rapp_mock.py
@@ -12,24 +12,20 @@ state = False
 
 while True:
     try:
-        state = not state # Alterna estado (True/False)
-
-        # Mensagem A1 via JSON
+        state = not state 
         policy = {
             "type": "A1_POLICY",
             "scope": "UE_GROUP_1",
             "value": state
         }
-
+        
         data_bytes = json.dumps(policy).encode('utf-8')
-
-        # Metodo PUT caracteristico da interface A1
         req = urllib.request.Request(url, data=data_bytes, method="PUT")
         req.add_header('Content-Type', 'application/json')
-
+        
         with urllib.request.urlopen(req) as r:
             print(f"[rApp] Enviado A1 Policy: Economia={state}")
-
+            
     except Exception as e:
         print(f"Erro ao contatar RIC: {e}")
 
